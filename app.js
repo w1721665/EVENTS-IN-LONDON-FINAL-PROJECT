91430077876c24ed9a9c2ad136ffd0ad10570abd
w1721665 <w1721665@my.westminster.ac.uk>
2021-11-29 15:53:22 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 const navSlide = () => {
-    const burger = document.querySelector('.burger)';
+    const burger = document.querySelector('.burger');
     const navLinks = document.querySelectorAll('.nav-links li');
     
     const nav = document.querySelector('.nav-links');
   
-  burger.addEventListener('click',()=>{
-      //Toggle Nav
-    nav.classList.toggle('nav-active');
+    burger.addEventListener('click',() => {
+       //Toggle Nav
+       nav.classList.toggle('nav-active');
   
-          //Animate Links
-          navLinks.forEach((link,index) => {
-              if (link.style.animation) {
+       //Animate Links
+       navLinks.forEach((link,index) => {
+          if (link.style.animation) {
                   link.style.animation = ''
           } else {
-              link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s`/;
+              link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s`;
     
             }
           });
           //Burger Animation
-          burgerclassList.toggle('toggle');
+          burger.classList.toggle('toggle');
         });
   
   }
