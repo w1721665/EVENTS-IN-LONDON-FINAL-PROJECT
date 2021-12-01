889aeb8677216bdf755808ff61dec35f230a3880
w1721665 <w1721665@my.westminster.ac.uk>
2021-12-01 18:31:40 +0000

    Update naturalhistorymuseumicerink.js

--- naturalhistorymuseumicerink.js
+++ naturalhistorymuseumicerink.js
@@ -38,5 +38,5 @@ function SlideShow(n) {
     slidePosition++;
     if (slidePosition > slides.length) { slidePosition = 1}
     slides[slidePosition-1].style.display = "block";
-    setTimeout(SlideShow, 2000); // Change image every 2 seconds
+    setTimeout(SlideShow, 2000); // Change image every 5 seconds
 }
