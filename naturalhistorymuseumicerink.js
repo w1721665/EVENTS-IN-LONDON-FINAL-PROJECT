d0eaa43fb41000d0f12a4d935c7bf0b40d021aed
w1721665 <w1721665@my.westminster.ac.uk>
2021-12-01 14:52:22 +0000

    Update naturalhistorymuseumicerink.js

--- naturalhistorymuseumicerink.js
+++ naturalhistorymuseumicerink.js
@@ -15,31 +15,12 @@ function currentSlide(n) {
 function SlideShow(n) {
   var i;
   var slides = document.getElementsByClassName("Containers");
-  var circles = document.getElementsByClassName("dots");
-  if (n>slides.length) {slidePosition = 1}
-  if (n < 1) {slidePosition = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  for (i=0; i <circles.length; i ++) {
-       circles[i].className = circles[i].className.replace("enable","");
-  }
-  slides[slidePosition-1].style.display = "block";
-  circles[slidePosition-1].className += "enable";
-}
-
-var slidePosition=0;
-SlideShow();
-
-function SlideShow() {
-  var i;
-  var slides = document.getElementsByClassName("Containers");
-  for (i =0; i < slides.length; i ++) {
-    slides[i].style.display= "none";
-  }
 
   slidePosition++;
-  if (slidePosition > slides.length) {slidePosition = 1}
-  slides[slidePosition-1].style.display = "block";
+  if (slidePosition > slides.length) { slidePosition = 1; }
+  slides[slidePosition - 1].style.display = "block";
   setTimeout(SlideShow, 2000); // Change image every 2 seconds
 }
