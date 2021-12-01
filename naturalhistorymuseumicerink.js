f48c3c6f90ade4ad9f4367749440077a725ce3fe
w1721665 <w1721665@my.westminster.ac.uk>
2021-12-01 16:31:18 +0000

    Update naturalhistorymuseumicerink.js

--- naturalhistorymuseumicerink.js
+++ naturalhistorymuseumicerink.js
@@ -1,9 +1,8 @@
- var slidePosition = 1;
+var slidePosition = 1;
 SlideShow(slidePosition);
 
 //forward/Back controls 
-function 
-plusSlides(n) {
+function plusSlides(n) {
   SlideShow(slidePosition += n);
 }
 
@@ -15,12 +14,29 @@ function currentSlide(n) {
 function SlideShow(n) {
   var i;
   var slides = document.getElementsByClassName("Containers");
+  var circles = document.getElementsByClassName ("dots");
+  if (n>slides.length) {slidePosition = 1}
+  if (n<1) {slidePosition = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
+  for (i = 0; i< circles.length; i++) {
+    circles[i].className = circles[i].className.replace("enable","");
+  }
+  slides[slidePosition-1].style.display = "block";
+  circles [slidePosition-1].className += "enable";
 
-  slidePosition++;
-  if (slidePosition > slides.length) { slidePosition = 1; }
-  slides[slidePosition - 1].style.display = "block";
-  setTimeout(SlideShow, 2000); // Change image every 2 seconds
+  var slidePosition = 0;
+  SlideShow();
+
+  function SlideShow() {
+    var i; 
+    var slides = document.getElementsByClassName("Containers");
+    for (i=0; i<slides.length); i++) {
+      slides[i].style.display="none";
+    }
+    slidePosition++;
+    if (slidePosition > slides.length) { slidePosition = 1}
+    slides[slidePosition-1].style.display = "block";
+    setTimeout(SlideShow, 2000); // Change image every 2 seconds
 }
