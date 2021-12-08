206f2dd8dfe907b9dd5c68422db8242fadc14d4a
manju1515 <w1723158@my.westminster.ac.uk>
2021-12-08 11:36:45 +0000

    Create backup website project.js

---
+++ backup website project.js
@@ -0,0 +1,26 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const navLinks = document.querySelectorAll('.nav-links li');
+    
+    const nav = document.querySelector('.nav-links');
+  
+    burger.addEventListener('click',() => {
+       //Toggle Nav
+       nav.classList.toggle('nav-active');
+  
+       //Animate Links
+       navLinks.forEach((link,index) => {
+          if (link.style.animation) {
+                  link.style.animation = ''
+          } else {
+              link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s`;
+    
+            }
+          });
+          //Burger Animation
+          burger.classList.toggle('toggle');
+        });
+  
+  }
+  
+  navSlide();
